0dbf3f4cc5c9b629a035adc1df8abbdbea2f45bd
mrDominik7 <mrdominik7@gmail.com>
2018-10-29 15:09:19 +0200

    files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+// import { info$ } from './data';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    public tmp;
+    public socialData;
+
+    onDataWeather(tmp: object) {
+        // console.log(tmp);
+        this.tmp = tmp;
+    }
+
+    onDataSocial(socialData: object) {
+        // console.log(socialData);
+        this.socialData = socialData;
+    }
+
+    ngOnInit() {
+    }
+
 }
+
